a638a441380ee518ffd384646d8c598b20eed4f8
Bmonkey <csalv.gonz@gmail.com>
2019-01-10 20:59:30 +0100

    Variable entotno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,12 @@
 /** ENTORNO */
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-/** CREDENCIALES */
-let userDB = 'cafe-user';
-let passDB = 'caf3pass';
-
 /** PUERTO */
 process.env.PORT = process.env.PORT || 3000;
 
 /** BBDD */
 let DBdev = 'mongodb://localhost:27017/cafe';
-let DBprod = 'mongodb://'+userDB+':'+passDB+'@ds135974.mlab.com:35974/cafe'
+let DBprod = 'mongodb://'+process.env.userDB+':'+process.env.passDB+ process.env.MONGO_URI;
 
 process.env.URLDB = (process.env.NODE_ENV === 'dev') ? DBdev: DBprod;
  
