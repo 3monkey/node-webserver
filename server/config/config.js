6b7b4106c17029b3ae55c818967d778797f349e1
Bmonkey <csalv.gonz@gmail.com>
2019-01-10 21:07:19 +0100

    Corrección

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,16 @@
 /** ENTORNO */
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+/** CREDENCIALES */
+let userDB = 'cafe-user';
+let passDB = 'caf3pass';
+
 /** PUERTO */
 process.env.PORT = process.env.PORT || 3000;
 
 /** BBDD */
 let DBdev = 'mongodb://localhost:27017/cafe';
-let DBprod = 'mongodb://'+process.env.userDB+':'+process.env.passDB+ process.env.MONGO_URI;
+let DBprod = process.env.MONGO_URI;
 
 process.env.URLDB = (process.env.NODE_ENV === 'dev') ? DBdev: DBprod;
  
